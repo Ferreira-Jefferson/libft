5880c86f7582b7cd28c60ebbe0384168d3fb2f31
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c2r1p5.42sp.org.br>
2025-07-11 11:25:55 -0300

    refact: ft_memset_

--- ft_memset.c
+++ ft_memset.c
@@ -6,22 +6,19 @@
 /*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 08:34:48 by jtertuli          #+#    #+#             */
-/*   Updated: 2025/07/11 09:55:21 by jtertuli         ###   ########.fr       */
+/*   Updated: 2025/07/11 11:24:44 by jtertuli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memset(void *dest, int c, unsigned int len)
 {
-	unsigned int	i;
 	void			*head;
 
 	head = dest;
-	i = 0;
-	while (i < len)
+	while (len--)
 	{
 		*(unsigned char *) dest = (unsigned char) c;
 		dest++;
-		i++;
 	}
 	return (head);
 }
