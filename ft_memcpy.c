51d9065af18b9fa8f351dfd5e8fc704a7d0be932
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c1r1p4.42sp.org.br>
2025-07-12 07:50:32 -0300

    refact: ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 11:35:57 by jtertuli          #+#    #+#             */
-/*   Updated: 2025/07/12 07:44:42 by jtertuli         ###   ########.fr       */
+/*   Updated: 2025/07/12 07:50:26 by jtertuli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
-	unsigned char *aux_dst;
-	unsigned char *aux_src;
+	unsigned char	*aux_dst;
+	unsigned char	*aux_src;
 
 	aux_dst = (unsigned char *) dst;
 	aux_src = (unsigned char *) src;
-	while(len--)
+	while (len--)
 	{
 		*aux_dst = *aux_src;
 		aux_dst++;
