736ed1add0ea0d68280bc68f11c1a0234bf13271
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c2r4p6.42sp.org.br>
2025-07-14 15:33:10 -0300

    add: ft_strdup

--- test.c
+++ test.c
@@ -29,6 +29,7 @@ void	test_ft_memcmp(void);
 void	test_ft_strstr(void);
 void	test_ft_atoi(void);
 void	test_ft_calloc(void);
+void	test_ft_strdup(void);
 
 int	main(void)
 {
@@ -58,9 +59,47 @@ int	main(void)
 	test_ft_strstr();
 	test_ft_atoi();
 	test_ft_calloc();
+	test_ft_strdup();
 
 	printf("\n*************** TESTS OK ************\n");
 	return (0);
+}
+
+void	test_ft_strdup(void)
+{
+	printf("ft_strdup: ");
+	char	*p;
+	char	str[] = "teste";
+
+	p = ft_strdup(str);
+	if (!p)
+	{
+		assert(p == NULL);
+		return ;
+	}
+	assert(p[0] == 't');
+	assert(p[4] == 'e');
+	assert(p[5] == '\0');
+	printf("OK \n");
+}
+
+void	test_ft_calloc(void)
+{
+	printf("ft_calloc: ");
+	int	*i;
+
+	i = ft_calloc(3, sizeof(int));
+	if (!i)
+	{
+		assert(i == NULL);
+		return ;
+	}
+	i[0] = 1;
+	i[1] = 2;
+	assert(i[0] == 1);
+	assert(i[1] == 2);
+	assert(i[2] == 0);
+	printf("OK \n");
 }
 
 void	test_ft_atoi(void)
@@ -102,25 +141,6 @@ void	test_ft_atoi(void)
 	assert(ft_atoi(s15) == 0); // Múltiplos sinais (o padrão é parar no segundo)
 	assert(ft_atoi(s16) == 0); // Múltiplos sinais (o padrão é parar no segundo)
 	assert(ft_atoi(s17) == 12); // Sinal no meio da string (atoi para antes)
-}
-
-void	test_ft_calloc(void)
-{
-	printf("ft_calloc: ");
-	int	*i;
-
-	i = ft_calloc(3, sizeof(int));
-	if (!i)
-	{
-		assert(i == NULL);
-		return ;
-	}
-	i[0] = 1;
-	i[1] = 2;
-	assert(i[0] == 1);
-	assert(i[1] == 2);
-	assert(i[2] == 0);
-	printf("OK \n");
 }
 
 void	test_ft_strstr(void)
