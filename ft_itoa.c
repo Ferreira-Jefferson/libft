505e842c53551c210897f3c2e75e6ab20a4ceacd
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c2r1p5.42sp.org.br>
2025-07-18 13:45:17 -0300

    add: ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 16:33:57 by jtertuli          #+#    #+#             */
-/*   Updated: 2025/07/18 13:40:32 by jtertuli         ###   ########.fr       */
+/*   Updated: 2025/07/18 13:44:48 by jtertuli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,20 @@ char	*ft_itoa(int n)
 	unsigned int	nb;
 	unsigned int	nb_count;
 	char			*s;
-	int 			count_decimal_places;
+	int				count_decimal_places;
 
 	nb = n;
 	if (n < 0)
 		nb = n * -1;
 	count_decimal_places = 1;
 	nb_count = nb;
-	while ((nb_count /= 10) > 0)
+	while (nb_count > 0)
+	{
 		count_decimal_places++;
+		nb_count /= 10;
+	}
 	s = (char *) malloc((count_decimal_places + (1 + (n < 0))) * sizeof(char));
-	if(n < 0)
+	if (n < 0)
 		s[0] = '-';
 	s[count_decimal_places + (n < 0)] = '\0';
 	while (count_decimal_places--)
@@ -35,6 +38,5 @@ char	*ft_itoa(int n)
 		s[count_decimal_places + (n < 0)] = (nb % 10) + '0';
 		nb /= 10;
 	}
-	return (s);	
+	return (s);
 }
-
