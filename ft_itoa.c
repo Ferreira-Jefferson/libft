e3241b3ca56f35df764670babfd1a62c667bf715
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c2r1p5.42sp.org.br>
2025-07-18 13:42:54 -0300

    add: ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,61 +6,35 @@
 /*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 16:33:57 by jtertuli          #+#    #+#             */
-/*   Updated: 2025/07/18 10:23:07 by jtertuli         ###   ########.fr       */
+/*   Updated: 2025/07/18 13:40:32 by jtertuli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	get_signal(int n, char **s, unsigned int *nb)
-{
-	if (n < 0)
-	{
-		*s = ft_strdup("-");
-		*nb = n * -1;
-	}
-	else
-	{
-		*s = ft_strdup("");
-		*nb = n;
-	}
-	return (*s != NULL);
-}
-
-char	*get_minus_ten(unsigned int nb, char *s)
-{
-	char	*ss;
-	char	num[2];
-
-	num[0] = nb + '0';
-	num[1] = '\0';
-	ss = ft_strjoin(s, num);
-	return (ss);
-}
-
 char	*ft_itoa(int n)
 {
-	char			*s;
-	char			*it;
-	char			*ss;
 	unsigned int	nb;
-	char			num[2];
+	unsigned int	nb_count;
+	char			*s;
+	int 			count_decimal_places;
 
-	if (!get_signal(n, &s, &nb))
-		return (NULL);
-	if (nb < 10)
+	nb = n;
+	if (n < 0)
+		nb = n * -1;
+	count_decimal_places = 1;
+	nb_count = nb;
+	while ((nb_count /= 10) > 0)
+		count_decimal_places++;
+	s = (char *) malloc((count_decimal_places + (1 + (n < 0))) * sizeof(char));
+	if(n < 0)
+		s[0] = '-';
+	s[count_decimal_places + (n < 0)] = '\0';
+	while (count_decimal_places--)
 	{
-		ss = get_minus_ten(nb, s);
-		free(s);
-		return (ss);
+		s[count_decimal_places + (n < 0)] = (nb % 10) + '0';
+		nb /= 10;
 	}
-	num[0] = (nb % 10) + '0';
-	num[1] = '\0';
-	it = ft_itoa(nb / 10);
-	ss = ft_strjoin(s, it);
-	free(s);
-	s = ft_strjoin(ss, num);
-	free(it);
-	free(ss);
-	return (s);
+	return (s);	
 }
+
